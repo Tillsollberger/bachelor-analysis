d1a0cfd66676667048b7956e34bd7ff8c465dfc7
Till <tillemil.sollberger@uzh.ch>
2025-07-28 00:28:02 +0200

    Third notebook added

---
+++ Analyse_Files/Answercategories.py
@@ -0,0 +1,56 @@
+# Answercategories.py
+
+# Answer order for: Usefullness AI
+usefulness_order = [
+    "Sehr nützlich",
+    "Ziemlich nützlich",
+    "Teils/teils",
+    "Wenig nützlich",
+    "Gar nicht nützlich"
+]
+
+# Answer order for: Deal with AI
+deal_order = [
+    "Sehr gut",
+    "Gut",
+    "Mittel",
+    "Schlecht",
+    "Sehr schlecht"
+]
+
+# Answer order for: Understanding AI
+understanding_order = [
+    "Sehr gut",
+    "Gut",
+    "Mittel",
+    "Schlecht",
+    "Sehr schlecht"
+]
+
+# Answer order for: Reliability AI
+reliability_order = [
+    "Sehr zuverlässig",
+    "Ziemlich zuverlässig",
+    "Teils/teils",
+    "Wenig zuverlässig",
+    "Gar nicht zuverlässig"
+]
+
+# Answer order for: Concerns AI
+concerns_order = [
+    "Ja, stark",
+    "Eher ja",
+    "Teils/teils",
+    "Eher nein",
+    "Nein"
+]
+
+# Answer order for: Help of AI
+helpfulness_order = [
+    "Sehr gut",
+    "Gut",
+    "Mittel",
+    "Schlecht",
+    "Sehr schlecht"
+]
+
